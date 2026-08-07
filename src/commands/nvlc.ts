f39ca98b6ac0c4bec0503b9193daa9bf43d5a9c8
Mrlion <87368344+L0stedMrlion@users.noreply.github.com>
2026-08-07 10:08:58 +0200

    Create nvlc.ts

---
+++ src/commands/nvlc.ts
@@ -0,0 +1,57 @@
+import type {
+  CommandData,
+  SlashCommandProps,
+  CommandOptions,
+} from 'commandkit';
+import {
+  MessageFlags,
+  TextDisplayBuilder,
+  ThumbnailBuilder,
+  SectionBuilder,
+} from 'discord.js';
+
+export const data: CommandData = {
+  name: 'nvlcfix',
+  description: 'Shows how to fix the Not a valid client connection error',
+  integration_types: [0, 1],
+  contexts: [0, 1, 2],
+};
+
+export async function run({ interaction }: SlashCommandProps) {
+  const textComponent = new TextDisplayBuilder().setContent(
+    '# 🔧 Lion Police Roleplay - Řešení chyby při připojení\n\n' +
+      'Pokud se Vám při připojování na server zobrazí následující chyba:\n\n' +
+      '```text\n' +
+      'Obtaining configuration from server failed.\n' +
+      'Error text: Not a valid client.\n' +
+      '```\n' +
+      '### ✅ Řešení tohoto problému\n' +
+      'Otevřete ve FiveM konzoli pomocí **F8** a zadejte následující command:\n\n' +
+      '```text\n' +
+      'connect roleplay.lionsproject.eu\n' +
+      '```\n' +
+      'Následně command potvrďte **Enter** a FiveM Vás připojí přímo na server.',
+  );
+
+  const thumbnailComponent = new ThumbnailBuilder({
+    media: {
+      url: 'https://cdn.discordapp.com/emojis/1292049154402549762.png',
+    },
+  });
+
+  const sectionComponent = new SectionBuilder()
+    .addTextDisplayComponents(textComponent)
+    .setThumbnailAccessory(thumbnailComponent);
+
+  await interaction.reply({
+    flags: MessageFlags.IsComponentsV2,
+    components: [sectionComponent],
+  });
+}
+
+export const options: CommandOptions = {
+  devOnly: false,
+  userPermissions: ['Administrator', 'AddReactions'],
+  botPermissions: ['Administrator', 'AddReactions'],
+  deleted: false,
+};
