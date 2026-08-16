a49581ce045510caac5237f9d60359d25e5b97b1
Mrlion <87368344+L0stedMrlion@users.noreply.github.com>
2026-08-16 15:02:31 +0200

    Update nve.ts

--- src/app/commands/(info)/nve.ts
+++ src/app/commands/(info)/nve.ts
@@ -91,8 +91,3 @@ export async function chatInput({ interaction }: ChatInputCommandContext) {
     }
   });
 }
-
-export const metadata: CommandMetadata = {
-  userPermissions: ['Administrator', 'AddReactions'],
-  botPermissions: ['Administrator', 'AddReactions'],
-};
