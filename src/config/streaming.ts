453a3542a59e79bc1228111f5e4075df7816dc1e
Mrlion <87368344+L0stedMrlion@users.noreply.github.com>
2026-08-30 23:13:46 +0200

    Update streaming.ts

--- src/config/streaming.ts
+++ src/config/streaming.ts
@@ -1,8 +1,6 @@
 export const STREAMER_ROLE_ID = '1287347387706118185';
 
 export const STREAM_URLS: Readonly<Record<string, string>> = {
-  '892296560845660212': 'https://kick.com/pentyyy',
   '769892516152999957': 'https://www.twitch.tv/petrpetr0',
-  '710549603216261141': 'https://www.twitch.tv/lostedmrlion',
-  '432501487361327114': 'https://kick.com/dezzyone',
+  '710549603216261141': 'https://www.twitch.tv/lostedmrlion'
 };
